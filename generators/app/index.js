cbf35bb211f98e9705aa604b9ea6fbca1c09854b
Sai Saran Vaidyanathan <ssvaidyanathan@google.com>
2021-09-16 15:17:47 -0700

    Fix

--- generators/app/index.js
+++ generators/app/index.js
@@ -242,12 +242,12 @@ module.exports = class extends Generator {
 	        {name : this.answers.name, version: this.answers.version}
 	     );
 	     this.fs.copyTpl(
-	        this.templatePath('.eslintrc.yml'),
+	        this.templatePath('eslintrc.yml'),
 	        this.destinationPath(`${this.answers.destination}/${this.answers.name}-${this.answers.version}/.eslintrc.yml`),
 	        {name : this.answers.name, version: this.answers.version}
 	     );
 	     this.fs.copyTpl(
-	        this.templatePath('.eslintrc-jsc.yml'),
+	        this.templatePath('eslintrc-jsc.yml'),
 	        this.destinationPath(`${this.answers.destination}/${this.answers.name}-${this.answers.version}/.eslintrc-jsc.yml`),
 	        {name : this.answers.name, version: this.answers.version}
 	     );
